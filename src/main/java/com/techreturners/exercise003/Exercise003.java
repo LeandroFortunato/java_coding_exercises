6564e6317a90cb8d5dc097e4413b820e6bb28f20
Leandro Fortunato <robertomax@yahoo.com>
2021-09-17 13:27:40 +0100

    Implement exercise 003 - getIceCreamCode

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,16 @@
 package com.techreturners.exercise003;
 
+import javax.print.FlavorException;
+
 public class Exercise003 {
+    private String flavor[] = {"Pistachio","Raspberry Ripple","Vanilla","Mint Chocolate Chip","Chocolate","Mango Sorbet"};
 
-    int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+    public int getIceCreamCode(String iceCreamFlavour) {
+         for (int i = 0; i < 6; i++) {
+             if (flavor[i] == iceCreamFlavour) {
+                return i;
+             }
+         }
+         return -1;
     }
-
-    String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
-    }
-
 }
