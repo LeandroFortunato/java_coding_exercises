06f4f91fe5c1ad68d44ae03c26d45cd9f7ae1f3a
Leandro Fortunato <robertomax@yahoo.com>
2021-09-17 06:53:52 +0100

    Implement exercise 005 - isPangram

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,18 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+
+        input = input.toUpperCase();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        boolean all_found = true;
+            
+        for (int i = 0; i < alphabet.length(); i++) {
+            if (input.indexOf(alphabet.substring(i,i+1)) == -1) {
+                all_found = false;
+                break;
+            };
+        }
+        return all_found;
     }
 
 }
